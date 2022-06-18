e806108f0564f09cca5b3e44aebcc2659822003e
Macart <86038701+TokomaBaou@users.noreply.github.com>
2022-06-18 22:17:31 +0900

    vowelsCount

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import "./styles.css";
 
 export const App = () => {
+  const getCount = (str: string) => {
+    let vowelsCount = 0;
+    vowelsCount = (str.match(/[aeiou]/gi) || []).length;
+    return vowelsCount;
+  };
+  getCount("abrakatabura");
   return (
     <div className="App">
+      {console.log(getCount)}
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
