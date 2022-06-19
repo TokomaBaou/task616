bcbaa6dd0b645a4d816c2520c6fc48aa8d5255f0
Macart <86038701+TokomaBaou@users.noreply.github.com>
2022-06-19 23:43:46 +0900

    even_or_odd

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,15 @@ export const App = () => {
     return vowelsCount;
   };
   getCount("abrakatabura");
+
+  // let number = 7;
+  const even_or_odd = (number: number) => {
+    return number % 2 ? "Odd" : "Even";
+  };
   return (
     <div className="App">
-      {console.log(getCount)}
+      {console.log(getCount("abrakatabura"))}
+      {console.log(even_or_odd(7))}
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
